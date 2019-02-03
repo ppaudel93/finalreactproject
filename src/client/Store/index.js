da19af276fc1fc6f34f1a9b26d8cee6f2e62df0c
Prayog <ppaudel93@gmail.com>
2019-02-03 11:01:09 +0545

    epics setup

--- src/client/Store/index.js
+++ src/client/Store/index.js
@@ -1,10 +1,13 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import CombineReducer from "../Reducers";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import rootEpic from "../Epics";
+import { createEpicMiddleware, EpicMiddleware } from "redux-observable";
 
 //const configureStore = ()
+const epicMiddleware = createEpicMiddleware();
 const store = createStore(
   CombineReducer,
-  composeWithDevTools(applyMiddleware())
+  composeWithDevTools(applyMiddleware(epicMiddleware))
 );
 export default store;
